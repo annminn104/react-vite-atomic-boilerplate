f4bda60a91db93326f0ff2d01c3fcb1629c9ccbe
Anh Minh (Matthew Nguyen) <76109800+annminn104@users.noreply.github.com>
2023-11-29 17:40:17 +0700

    build: init dockerfile docker compose ngix and husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,33 +2,35 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "eslint-config-prettier",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
+    'prettier'
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.ts"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "prettier"],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    "prettier/prettier": [
-      "warn",
+    '@typescript-eslint/no-unused-vars': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-no-target-blank': 'warn',
+    'react/prop-types': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': [
+      'warn',
       {
-        arrowParens: "always",
+        arrowParens: 'always',
         semi: false,
-        trailingComma: "none",
+        trailingComma: 'none',
         tabWidth: 2,
-        endOfLine: "auto",
+        endOfLine: 'auto',
         useTabs: false,
         singleQuote: true,
         printWidth: 120,
-        jsxSingleQuote: true,
-      },
-    ],
-  },
-};
+        jsxSingleQuote: true
+      }
+    ]
+  }
+}
